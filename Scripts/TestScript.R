1ec2007f2973cdeca306db4b54a26fee48e3a34d
Mike Sullivan <MikeJSullivan@gmail.com>
2019-12-06 12:44:07 -0600

    Modified the second print statement and added a third.

--- Scripts/TestScript.R
+++ Scripts/TestScript.R
@@ -1,3 +1,5 @@
 print("This file was created within RStudio")
 
-print("And now it lives in GitHub")
+print("And now it lives in GitHub!")
+
+print("Here I added a new line to the script, after initial commit.")
